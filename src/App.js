8ad809577745409a7774809204c73dfdfc93d35c
fares1994 <fhalasahhh@gmail.com>
2021-02-07 11:08:10 +0200

    adding pages files and adding router in the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
+import SignIn from './pages/signIn.page';
+import SignUp from './pages/signup.page';
+import User from './pages/user.page';
 
 const App = () => {
   return (
-    <div className="App">
-      works
-    </div>
+    <Router>
+      <Switch>
+       <Route path='/signin' exact>
+        <SignIn/>
+       </Route>
+       <Route path='/signup' exact>
+         <SignUp/>
+       </Route>
+       <Route path='/user/:userid' exact>
+         <User/>
+       </Route>
+       <Redirect to="/signin" />
+       </Switch>
+    </Router>
   );
 }
 
